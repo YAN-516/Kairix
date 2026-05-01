980053cc8eae582da449c401605e436e38754723
suzume <250745208@qq.com>
2026-05-01 11:02:44 +0000

    准备合并

--- os/src/main.rs
+++ os/src/main.rs
@@ -312,7 +312,7 @@ fn main(id: usize, first: bool) -> bool {
         //     println!("int enabled");
         // }
 
-        net::init();
+        // net::init();
         init_processors();
         println!("cpu {} init processors", id);
 
