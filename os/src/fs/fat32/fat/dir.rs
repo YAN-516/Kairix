e61befd8e6a47320891d5345ac445e0c74f68437
YAN <1748323932@qq.com>
2026-05-13 02:35:58 +0000

    接入fat32，思考mount怎么实现

--- os/src/fs/fat32/fat/dir.rs
+++
@@ -1,5 +0,0 @@
-use fatfs::Dir;
-use crate::fs::fat32::io::FatIoAdapter;
-use fatfs::{LossyOemCpConverter,NullTimeProvider};
-pub struct Fat32Dir(Dir<'static, FatIoAdapter, NullTimeProvider, LossyOemCpConverter>);
-
