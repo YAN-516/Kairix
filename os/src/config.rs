962735eb9c8ed3af5d694b24b4cccc50e4d7a22f
YAN <1748323932@qq.com>
2026-05-31 07:16:07 +0000

    加入超时机制

--- os/src/config.rs
+++ os/src/config.rs
@@ -41,3 +41,6 @@ pub const MMAP_BASE: usize = 0x4000_0000;
 pub use crate::board::{_CLOCK_FREQ, MEMORY_END, MMIO};
 
 pub const BLOCK_SIZE: usize = 512;
+
+pub const LTP_WATCHDOG_TIMEOUT_SECS: u64 = 30;
+pub const LTP_WATCHDOG_PATH_FRAGMENT: &str = "/ltp/testcases/bin/";
