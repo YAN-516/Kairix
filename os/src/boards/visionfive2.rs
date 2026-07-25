687c393d1673e1db61cd90052997f17d17f01474
Starry <2813498706@qq.com>
2026-07-25 06:33:00 +0000

    准备专心debug dwmac

--- os/src/boards/visionfive2.rs
+++ os/src/boards/visionfive2.rs
@@ -3,10 +3,15 @@ pub const _CLOCK_FREQ: usize = 4_000_000;
 #[allow(unused)]
 pub const MMIO: &[(usize, usize)] = &[
     (0x0200_0000, 0x01_0000), // ACLINT/CLINT area reported by OpenSBI
+    (0x0201_0000, 0x00_4000), // SiFive L2 cache controller
     (0x0c00_0000, 0x40_0000), // PLIC
     (0x1000_0000, 0x00_1000), // UART0
+    (0x1302_0000, 0x00_1000), // system clock/reset controller
     (0x1601_0000, 0x00_1000), // sdio0
     (0x1602_0000, 0x00_1000), // sdio1
+    (0x1603_0000, 0x01_0000), // GMAC0 (DWMAC 5.10a)
+    (0x1700_0000, 0x00_1000), // always-on clock/reset controller
+    (0x1701_0000, 0x00_1000), // always-on syscon (GMAC0 PHY interface select)
 ];
 
 pub type BlockDeviceImpl = crate::drivers::block::Vf2SdBlock;
